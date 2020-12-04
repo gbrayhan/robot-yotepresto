c7beb5f130583192dec255e574da98a43e186a16
Alex Guerrero <brayhan.gabriel@wizeline.com>
2020-12-03 19:32:16 -0600

    Ir a invertir

--- main.py
+++ main.py
@@ -18,10 +18,12 @@ browser.get(urlLogin)
 # click email input
 emailArea = browser.find_element_by_id('email-field-clone')
 emailArea.click()
+
+# enter email
 emailYTP = env("YTP_EMAIL")
 for character in emailYTP:
     emailArea.send_keys(character)
-    time.sleep(0.1)
+    time.sleep(0.15)
 emailArea.click()
 
 continueButton = browser.find_elements_by_xpath("//input[@name='commit' and @value='Continuar']")[0]
@@ -31,16 +33,16 @@ time.sleep(1)
 
 # click email password
 password = browser.find_element_by_id('sessions_password')
-time.sleep(0.1)
+time.sleep(0.15)
 password.click()
-time.sleep(0.1)
+time.sleep(0.15)
 
 # enter pass
 passYTP = env("YTP_PASS")
 for character in passYTP:
     password.send_keys(character)
-    time.sleep(0.1)
-time.sleep(0.1)
+    time.sleep(0.15)
+time.sleep(0.15)
 password.click()
 
 # click pass
@@ -51,4 +53,18 @@ iniciarButton.click()
 time.sleep(3)
 quantity = browser.find_elements_by_xpath("//div[@class='quantity' and @data-testid='fundingQuantity']")[0].text
 
+acceptedQualification = ["A1", "A2", "A3", "A4", "A5", "A6", "A7",
+                         "B1", "B2", "B3", "B4", "B5", "B6", "B7", "C1"]
+
+if int(quantity) > 0:
+    solicitudesExistentes = browser.find_element_by_xpath(
+        "//*[contains(text(), 'Ir a prestar')]").find_element_by_xpath("..")
+    solicitudesExistentes.click()
+    time.sleep(2)
+    qualification = browser.find_elements_by_xpath("//span[@class='qualification']")[0].text
+    if qualification in acceptedQualification:
+        print("Send EMAIL")
+
 print("El numero de solicitudes activas es: " + quantity)
+time.sleep(5)
+browser.close()
